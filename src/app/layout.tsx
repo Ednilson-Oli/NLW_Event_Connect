9853315f85be20c91a9d99aed7f83c62e9b4f300
Ednilson-Oli <ednilsonoliveira2@hotmail.com>
2025-02-18 22:48:56 -0300

    feat: Adicionado suporte ao Biome para formatação e linting, e atualizado componentes com melhorias de estilo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import './globals.css';
-import type { Metadata } from "next";
-import {Montserrat , Oxanium} from 'next/font/google'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "devstage",
-};
+  title: 'devstage',
+}
 const oxanium = Oxanium({
-  weight:['500', '600'],
-  subsets:['latin'],
+  weight: ['500', '600'],
+  subsets: ['latin'],
   variable: '--font-oxanium',
 })
 
 const montserrat = Oxanium({
-  weight:['400', '600'],
-  subsets:['latin'],
+  weight: ['400', '600'],
+  subsets: ['latin'],
   variable: '--font-Montserrat',
 })
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={`${montserrat.variable} ${oxanium.variable}`}>
-      <body className='bg-gray-900 text-gray-100 antialiased'>{children}</body>
+      <body className="bg-gray-900 text-gray-100
+       antialiased bg-[url(/Background.png)] bg-no-repeat bg-top md:bg-right-top"
+       >
+        {children}
+      </body>
     </html>
-  );
+  )
 }
